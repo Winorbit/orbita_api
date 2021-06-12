d580f1e047dc0badf7cc0efb12d2ffca42f1fb50
Egorich42 <e.g.hutter@gmail.com>
2021-06-12 21:25:12 +0300

    add numbers and groups to lessons

--- api/models.py
+++ api/models.py
@@ -3,26 +3,21 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from tinymce import models as tinymce_models
 
-class Lesson(models.Model):
-    title = models.CharField(max_length=250, blank=False, default="")
-    source_link = models.CharField(max_length=2500, default='', blank=False)
-    lesson_description = tinymce_models.HTMLField()
-    lesson_usefull_links = tinymce_models.HTMLField()
-    lesson_homework = tinymce_models.HTMLField()
-    lesson_conspect = tinymce_models.HTMLField()
 
-    class Meta:
-        managed = True
-        db_table = 'Lessons'
-        verbose_name_plural = "Lessons"
+
+class UserProfile(models.Model):
+    profile_image = models.ImageField(upload_to="user_pics", blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    groups = ArrayField(models.CharField(max_length=10, blank=True), blank=True, default=list)
 
     def __str__(self):
-        return self.title
+        return self.user.username
 
 
 class Group(models.Model):
     title = models.CharField(max_length=350, blank=False, default="")
     discord_chat_link = models.CharField(max_length=1000, default='', blank=False)
+    group_number = models.IntegerField(default=1)
 
     class Meta:
         managed = True
@@ -33,13 +28,6 @@ class Group(models.Model):
         return self.title
 
 
-class UserProfile(models.Model):
-    profile_image = models.ImageField(upload_to="user_pics", blank=True, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    groups = ArrayField(models.CharField(max_length=10, blank=True), blank=True, default=list)
-
-    def __str__(self):
-        return self.user.username
 
 class Article(models.Model):
     title = models.CharField(max_length=250, blank=False, default="")
@@ -53,3 +41,21 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+class Lesson(models.Model):
+    title = models.CharField(max_length=250, blank=False, default="")
+    source_link = models.CharField(max_length=2500, default='', blank=False)
+    lesson_description = tinymce_models.HTMLField(blank=True, default="Здесь пока ничего нет")
+    lesson_usefull_links = tinymce_models.HTMLField(blank=True)
+    lesson_homework = tinymce_models.HTMLField(blank=True)
+    lesson_conspect = tinymce_models.HTMLField(blank=True)
+    lesson_number = models.IntegerField(default=1,unique=False,null=True)
+    lesson_group = models.OneToOneField(Group, on_delete=models.CASCADE, blank=True, unique=False,null=True)
+    
+
+    class Meta:
+        managed = True
+        db_table = 'Lessons'
+        verbose_name_plural = "Lessons"
+
+    def __str__(self):
+        return self.title
